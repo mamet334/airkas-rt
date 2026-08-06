8772a15483c88ca1e9d7db68ed17f2cb107044c9
mamet334 <andreanastasya798@gmail.com>
2026-08-06 10:58:45 +0700

    test: rewrite clean test files for billing and cycle engine

--- src/utils/billingEngine.test.js
+++ src/utils/billingEngine.test.js
@@ -6,18 +6,18 @@ const stateMock = {
   warga: [
     { id: 1, nama: 'Normal', aktif: true, adalah_pengelola: false, alamat: 'Rumah' },
     { id: 2, nama: 'Pengelola', aktif: true, adalah_pengelola: true, alamat: 'Rumah' },
-    { id: 3, nama: 'SISTEM', aktif: true, adalah_pengelola: false, alamat: 'SISTEM' },
+    { id: 3, nama: 'SISTEM', aktif: true, adalah_pengelola: false, alamat: 'SISTEM' }
   ],
   meteran: [
     { id: 'm1', warga_id: 1, bulan: 1, tahun: 2026, total_tagihan: 100000 },
     { id: 'm2', warga_id: 2, bulan: 1, tahun: 2026, total_tagihan: 80000 },
-    { id: 'm3', warga_id: 1, bulan: 2, tahun: 2026, total_tagihan: 100000 },
+    { id: 'm3', warga_id: 1, bulan: 2, tahun: 2026, total_tagihan: 100000 }
   ],
   pembayaran: [
     { id: 'p1', warga_id: 1, meteran_id: 'm1', bulan: 1, tahun: 2026, jumlah_bayar: 100000 },
     { id: 'p2', warga_id: 2, meteran_id: 'm2', bulan: 2, tahun: 2026, jumlah_bayar: 50000 },
     { id: 'p3', warga_id: 1, meteran_id: 'm3', bulan: 1, tahun: 2026, jumlah_bayar: 200000 },
-    { id: 'p4', warga_id: 3, meteran_id: 'mSys', bulan: 1, tahun: 2026, jumlah_bayar: 500000 },
+    { id: 'p4', warga_id: 3, meteran_id: 'mSys', bulan: 1, tahun: 2026, jumlah_bayar: 500000 }
   ]
 };
 
